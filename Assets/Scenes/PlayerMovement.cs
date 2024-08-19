0ad2dce21fe57d0467fef2ca420afccfd2b02cb9
GHGFaber <moises.fuentes904@gmail.com>
2024-08-18 18:23:56 -0700

    added player sprite, collision, and fixed transform when switching directions

--- Assets/Scenes/PlayerMovement.cs
+++ Assets/Scenes/PlayerMovement.cs
@@ -56,7 +56,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Flip(){
-        if (isFacingRight && horizontal < 0f || isFacingRight && horizontal > 0f){
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f){
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
