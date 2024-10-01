b9189615e3283e11f3f3a1df3b9a75b9368dda36
John Tran <johntran038@gmail.com>
2024-10-01 18:28:04 -0500

    Made tutorial better

--- Assets/Scripts/MessageUI.cs
+++ Assets/Scripts/MessageUI.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MessageUI : MonoBehaviour
 {
     [SerializeField] private MessageUI nextMessage;
+    public bool triggeredFinish = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,8 @@ public class MessageUI : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Return)) {
             if(nextMessage){
                 nextMessage.gameObject.SetActive(true);
+            }else{
+                triggeredFinish = true;
             }
             this.gameObject.SetActive(false);
         }
